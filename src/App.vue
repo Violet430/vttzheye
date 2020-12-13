5917cf98b0225c2ed27f662dd45ca5e1444ca39c
wuyee <1525625981@qq.com>
2020-12-13 22:41:25 +0800

    Init pojo: add tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class=" w-14 h-14 bg-green-400"></div>
 </template>
 
 <script lang="ts">
@@ -10,8 +11,8 @@ import HelloWorld from "./components/HelloWorld.vue";
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 });
 </script>
 
